26bb01d928b085333819b027ad57622163cbcb84
devansh9437njit <ds225@njit.edu>
2021-05-09 09:21:46 +0500

    Feature: Created init.sql using a CSV file and then converting it to SQL syntax

---
+++ db/init.sql
@@ -0,0 +1,267 @@
+CREATE TABLE `movies` (
+  `ReleaseYear` int DEFAULT NULL,
+  `Score` varchar(50) DEFAULT NULL,
+  `Title` varchar(50) DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+)
+
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1968, '  86', ' Greetings', 1);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1970, '  73', ' Hi', 3);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1971, '  40', ' Born to Win', 4);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1973, '  98', ' Mean Streets', 5);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1973, '  88', ' Bang the Drum Slowly', 6);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1974, '  97', ' The Godfather', 7);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1976, '  41', ' The Last Tycoon', 8);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1976, '  99', ' Taxi Driver', 9);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1977, '  47', ' 1900', 10);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1977, '  67', ' New York', 11);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1978, '  93', ' The Deer Hunter', 12);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1980, '  97', ' Raging Bull', 13);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1981, '  75', ' True Confessions', 14);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1983, '  90', ' The King of Comedy', 15);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1984, '  89', ' Once Upon a Time in America', 16);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1984, '  60', ' Falling in Love', 17);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1985, '  98', ' Brazil', 18);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1986, '  65', ' The Mission', 19);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1987, ' 100', ' Dear America: Letters Home From Vietnam', 20);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1987, '  80', ' The Untouchables', 21);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1987, '  78', ' Angel Heart', 22);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1988, '  96', ' Midnight Run', 23);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1989, '  64', ' Jacknife', 24);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1989, '  47', ' We''re No Angels', 25);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1990, '  88', ' Awakenings', 26);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1990, '  29', ' Stanley & Iris', 27);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1990, '  96', ' Goodfellas', 28);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1991, '  76', ' Cape Fear', 29);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1991, '  69', ' Mistress', 30);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1991, '  65', ' Guilty by Suspicion', 31);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1991, '  71', ' Backdraft', 32);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1992, '  87', ' Thunderheart', 33);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1992, '  67', ' Night and the City', 34);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1993, '  75', ' This Boy''s Life', 35);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1993, '  78', ' Mad Dog and Glory', 36);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1993, '  96', ' A Bronx Tale', 37);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1994, '  39', ' Mary Shelley''s Frankenstein', 38);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1995, '  80', ' Casino', 39);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1995, '  86', ' Heat', 40);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1996, '  74', ' Sleepers', 41);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1996, '  38', ' The Fan', 42);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1996, '  80', ' Marvin''s Room', 43);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1997, '  85', ' Wag the Dog', 44);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1997, '  87', ' Jackie Brown', 45);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1997, '  72', ' Cop Land', 46);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1998, '  68', ' Ronin', 47);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1998, '  38', ' Great Expectations', 48);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1999, '  69', ' Analyze This', 49);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(1999, '  43', ' Flawless', 50);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2000, '  43', ' The Adventures of Rocky & Bullwinkle', 51);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2000, '  84', ' Meet the Parents', 52);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2000, '  41', ' Men of Honor', 53);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2001, '  73', ' The Score', 54);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2001, '  33', ' 15 Minutes', 55);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2002, '  48', ' City by the Sea', 56);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2002, '  27', ' Analyze That', 57);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2003, '   4', ' Godsend', 58);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2004, '  35', ' Shark Tale', 59);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2004, '  38', ' Meet the Fockers', 60);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2005, '   4', ' The Bridge of San Luis Rey', 61);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2005, '  46', ' Rent', 62);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2005, '  13', ' Hide and Seek', 63);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2006, '  54', ' The Good Shepherd', 64);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2007, '  21', ' Arthur and the Invisibles', 65);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2007, '  76', ' Captain Shakespeare', 66);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2008, '  19', ' Righteous Kill', 67);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2008, '  51', ' What Just Happened?', 68);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2009, '  46', ' Everybody''s Fine', 69);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2010, '  72', ' Machete', 70);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2010, '  10', ' Little Fockers', 71);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2010, '  50', ' Stone', 72);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2011, '  25', ' Killer Elite', 73);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2011, '   7', ' New Year''s Eve', 74);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2011, '  70', ' Limitless', 75);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2012, '  92', ' Silver Linings Playbook', 76);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2012, '  51', ' Being Flynn', 77);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2012, '  29', ' Red Lights', 78);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2013, '  46', ' Last Vegas', 79);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2013, '   7', ' The Big Wedding', 80);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2013, '  29', ' Grudge Match', 81);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2013, '  11', ' Killing Season', 82);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2014, '   9', ' The Bag Man', 83);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2015, '  60', ' Joy', 84);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2015, '  26', ' Heist', 85);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2015, '  61', ' The Intern', 86);
+INSERT INTO movies
+(ReleaseYear, Score, Title, id)
+VALUES(2016, '  11', ' Dirty Grandpa', 87);
+
